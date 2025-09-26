11533edb9625f9dfd13cd462c8bfc7ba37db45bc
trumantoy <sl.truman@live.com>
2025-09-26 17:37:03 +0800

    feat: up功能

--- src/simtoy/data/stocraft/stocraft.py
+++ src/simtoy/data/stocraft/stocraft.py
@@ -10,6 +10,7 @@ import requests
 
 from datetime import datetime,timedelta
 from requests.exceptions import ReadTimeout
+from queue import Queue
 
 import io
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
@@ -138,87 +139,99 @@ def sync(args : DictProxy=dict(),records : DictProxy=dict()):
     args['status'] = '数据采集完成'
     return 60 * 60
 
-def feature(code,days=7):
-    date_end = datetime.now().date()
-    date_start = datetime.now().date() - timedelta(days=days)
+def feature(code,date):
+    特征 = pd.DataFrame(columns=['起点','终点','代价','涨幅'])
+    
+    transaction_filepath = os.path.join(db_dir,f'{code}-{date}-交易.csv')
+    # info_filepath = os.path.join(db_dir,f'{code}-{date}-行情.csv')
+    # rank_filepath = os.path.join(db_dir,f'{code}-{date}-人气.csv')
+    
+    if not os.path.exists(transaction_filepath):
+        return None
 
-    特征 = pd.DataFrame(columns=['时间','买卖比','买总额','卖总额','涨幅','小额占比','中额占比','大额占比','巨额占比'])
-    for date_cur in pd.date_range(date_start,date_end,freq='1D'):
-        file_date = date_cur.date().strftime('%Y%m%d')
-        transaction_filepath = os.path.join(db_dir,f'{code}-{file_date}-交易.csv')
-        info_filepath = os.path.join(db_dir,f'{code}-{file_date}-信息.csv')
-        rank_filepath = os.path.join(db_dir,f'{code}-{file_date}-人气.csv')
-   
-        if not os.path.exists(transaction_filepath):
+    交易 = pd.read_csv(transaction_filepath)
+    # 信息 = pd.read_csv(info_filepath)
+    # 人气 = pd.read_csv(rank_filepath)
+    基点 = 5.70 #昨收
+    起点 = 基点
+    
+    for i,r in 交易.iterrows():
+        时间,成交价,手数,买卖盘性质 = r['时间'],r['成交价'],r['手数'],r['买卖盘性质']
+
+        if 买卖盘性质 == '中性盘':
             continue
 
-        交易 = pd.read_csv(transaction_filepath)
-        信息 = pd.read_csv(info_filepath)
-        # 人气 = pd.read_csv(rank_filepath)
+        print(r.to_list())
 
-        # 人气['time'] = pd.to_datetime(file_date + ' ' + 人气['时间'])
-        # 人气 = 人气.set_index('time').between_time('9:20','15:0').reset_index(drop=True)
-        # 人气['时间'] = file_date + ' ' + 人气['时间']
-        # 人气['热度'] = np.log2(人气['排名'])
+        if 时间 != '09:25:00':
+            终点 = 成交价
 
-        信息.loc[len(信息.index)] = ['日期',file_date]
-        交易['时间'] = file_date + ' ' + 交易['时间']
-
-        交易 = 交易[(交易['买卖盘性质'] != '中性盘')].copy()
-        if 0 == 交易.shape[0]: continue
-        交易['金额'] = 交易['成交价'] * 交易['手数'] * 100
-        买盘 = 交易[交易['买卖盘性质'] == '买盘']
-        卖盘 = 交易[交易['买卖盘性质'] == '卖盘']
-        买总手 = round(买盘['手数'].sum())
-        卖总手 = round(卖盘['手数'].sum())
-        买总额 = round(买盘['金额'].sum() / 1e8,1)
-        卖总额 = round(卖盘['金额'].sum() / 1e8,1)
-        昨收价 = float(信息[信息['item'] == '昨收'].iloc[0]['value'])
-        涨幅 = round((交易['成交价'].iloc[-1] / 昨收价 - 1) * 100,2)
-
-        总额 = (买总额 + 卖总额) * 1e4
-        金额 = 交易['金额'] / 1e4
-        小额占比 = 金额[(金额 < 10)].sum() / 总额
-        中额占比 = 金额[(10 < 金额) & (金额 < 50)].sum() / 总额
-        大额占比 = 金额[(50 < 金额) & (金额 < 100)].sum() / 总额
-        巨额占比 = 金额[(100 < 金额)].sum() / 总额
-
-        特征.loc[len(特征.index)] = [
-            file_date,round(买总手/卖总手 if 卖总手>0 else 0,2),
-            买总额,卖总额,涨幅,
-            round(小额占比,2),round(中额占比,2),
-            round(大额占比,2),round(巨额占比,2)]
-    return 特征
-
-def up(args,records,up_records : dict):
-    df_records = pd.DataFrame(dict(zip(records.keys(),records.values())))
-    df_up_records = pd.DataFrame(columns=['代码','名称','涨幅','流通市值','评分'])
-
-
-    for i,row in df_records.iterrows(): 
-        code = row['代码']
-        name = row['名称']
-
-        feature_filepath = os.path.join(db_dir,f'{code}-0-特征.csv')
-        if os.path.exists(feature_filepath) and datetime.now().date() == datetime.fromtimestamp(os.path.getmtime(feature_filepath)).date():
-            特征 = pd.read_csv(feature_filepath)
         else:
-            特征 = feature(code)
-            特征.to_csv(feature_filepath)
+            起点 = 成交价
+            代价 = 成交价 * 手数
+            涨幅 = (终点 - 起点) / 基点 * 100
+            特征[len(特征.index)] = [起点,终点,代价,涨幅]
+    return 特征
+    # feature_filepath = os.path.join(db_dir,f'{code}-0-特征.csv')
+    # if os.path.exists(feature_filepath) and datetime.now().date() == datetime.fromtimestamp(os.path.getmtime(feature_filepath)).date():
+    #     特征 = pd.read_csv(feature_filepath)
+    # else:
+    #     特征 = feature(code)
+    #     特征.to_csv(feature_filepath)
+    # a = sum(特征['买卖比'] > 1.5)
+    # b = sum(特征['巨额占比'] > 0.5)
+    # 涨跌 = 特征['涨幅'].iloc[-1]
+    # c = 1 if 0 < 涨跌 < 5 else -1
+    # d = 2 if 60 < row['流通市值'] / 1e8 < 150 else -2
+    # s = a + b + c + d
+    # df_up_records.loc[len(df_up_records.index)] = [code,name,涨跌,round(row['流通市值'] / 1e8,1),s]
 
-        a = sum(特征['买卖比'] > 1.5)
-        b = sum(特征['巨额占比'] > 0.5)
-        涨跌 = 特征['涨幅'].iloc[-1]
-        c = 1 if 0 < 涨跌 < 5 else -1
-        d = 2 if 60 < row['流通市值'] / 1e8 < 150 else -2
 
-        s = a + b + c + d
-        df_up_records.loc[len(df_up_records.index)] = [code,name,涨跌,round(row['流通市值'] / 1e8,1),s]
+    # 人气['time'] = pd.to_datetime(file_date + ' ' + 人气['时间'])
+    # 人气 = 人气.set_index('time').between_time('9:20','15:0').reset_index(drop=True)
+    # 人气['时间'] = file_date + ' ' + 人气['时间']
+    # 人气['热度'] = np.log2(人气['排名'])
 
-        if i % 10 == 0:
-            up_records.update(df_up_records.to_dict(orient='list'))
-    return 60*60
+    # 信息.loc[len(信息.index)] = ['日期',file_date]
+    # 交易['时间'] = file_date + ' ' + 交易['时间']
 
+    # 交易 = 交易[(交易['买卖盘性质'] != '中性盘')].copy()
+    # if 0 == 交易.shape[0]: continue
+    # 交易['金额'] = 交易['成交价'] * 交易['手数'] * 100
+    # 买盘 = 交易[交易['买卖盘性质'] == '买盘']
+    # 卖盘 = 交易[交易['买卖盘性质'] == '卖盘']
+    # 买总手 = round(买盘['手数'].sum())
+    # 卖总手 = round(卖盘['手数'].sum())
+    # 买总额 = round(买盘['金额'].sum() / 1e8,1)
+    # 卖总额 = round(卖盘['金额'].sum() / 1e8,1)
+    # 昨收价 = float(信息[信息['item'] == '昨收'].iloc[0]['value'])
+    # 涨幅 = round((交易['成交价'].iloc[-1] / 昨收价 - 1) * 100,2)
+
+    # 总额 = (买总额 + 卖总额) * 1e4
+    # 金额 = 交易['金额'] / 1e4
+    # 小额占比 = 金额[(金额 < 10)].sum() / 总额
+    # 中额占比 = 金额[(10 < 金额) & (金额 < 50)].sum() / 总额
+    # 大额占比 = 金额[(50 < 金额) & (金额 < 100)].sum() / 总额
+    # 巨额占比 = 金额[(100 < 金额)].sum() / 总额
+
+    # 特征.loc[len(特征.index)] = [
+    #     file_date,round(买总手/卖总手 if 卖总手>0 else 0,2),
+    #     买总额,卖总额,涨幅,
+    #     round(小额占比,2),round(中额占比,2),
+    #     round(大额占比,2),round(巨额占比,2)]
+    # return 特征
+
+def up(worker_req : mp.Queue,worker_res : mp.Queue,dates):
+    df_up_records = pd.DataFrame(columns=['代码','名称','涨幅','流通市值','评分'])
+    stocks = get_stock_spot()
+    stocks = stocks[stocks['名称'].isin(['步步高','景兴纸业'])]    
+    for date in dates:
+        stocks.apply(lambda r: worker_req.put(('feature',r['代码'],date)),axis=1)
+
+    for _ in range(stocks.shape[0] * len(dates)):
+        _,code,date,val = worker_res.get()
+        print(code,date)
+    
 def get_stock_spot():
     h15 = datetime.now().replace(hour=15, minute=0, second=0)
 
@@ -227,15 +240,14 @@ def get_stock_spot():
     try:
         if os.path.exists(stocks_file_path) and datetime.now().date() == datetime.fromtimestamp(os.path.getmtime(stocks_file_path)).date():
             stocks = pd.read_csv(stocks_file_path,dtype={'代码':str})
-        elif datetime.now().weekday() < 5 and h15 < datetime.now():
+        elif not os.path.exists(stocks_file_path) or datetime.now().weekday() < 5 and h15 < datetime.now():
             stocks = ak.stock_zh_a_spot_em()
-            
             condition = ((stocks['代码'].str.startswith('00')) | (stocks['代码'].str.startswith('60'))) & \
                         (~stocks['名称'].str.startswith('PT')) & \
                         (~stocks['名称'].str.startswith('ST')) & \
                         (~stocks['名称'].str.startswith('*ST')) & \
                         (~stocks['最新价'].isnull()) & \
-                        (stocks['换手率'] > 0)
+                        (stocks['换手率'] > 0.001)
             stocks = stocks[condition].reset_index(drop=True)
             stocks.to_csv(stocks_file_path,index=False)
             spot_filepath = os.path.join(db_dir,f'0-{datetime.now().strftime("%Y%m%d")}-行情.csv')
@@ -253,19 +265,10 @@ def get_stock_spot():
 
 def get_stock_intraday(code,date):
     try:
-        filepath = os.path.join(db_dir,f'{code}-{date}-信息.csv')
+        filepath = os.path.join(db_dir,f'{code}-{date}-行情.csv')
         if not os.path.exists(filepath) or 0 == os.path.getsize(filepath):
-            info = ak.stock_individual_info_em(code,10)
-            if info[info['item'] == '总市值'].iloc[0]['value'] == '-':
-                return True
-
-            if info[info['item'] == '股票代码'].iloc[0]['value'] != code:
-                return True
-            
             bid = ak.stock_bid_ask_em(symbol=code)
-            info.loc[len(info.index)] = ['昨收',bid[bid['item'] == '昨收'].iloc[0]['value']]
-            info.loc[len(info.index)] = ['今开',bid[bid['item'] == '今开'].iloc[0]['value']]
-            info.to_csv(filepath,index=False)
+            bid[20:].to_csv(filepath,index=False)
 
         filepath = os.path.join(db_dir,f'{code}-{date}-交易.csv')
         if not os.path.exists(filepath) or 0 == os.path.getsize(filepath):
@@ -288,29 +291,24 @@ def get_stock_intraday(code,date):
 
     return True
 
-def worker(id,req : Queue,res : Queue):
+def worker(id,req : mp.Queue,res : mp.Queue):
     while True:
-        fun,code,date,sleep = req.get()
+        args = req.get()
     
-        if fun == 'sync':
-            print(req.qsize(),code,flush=True)
-            if not get_stock_intraday(code,date):
-                req.put((fun,code,date,10))
+        if args[0] == 'sync':
+            if not get_stock_intraday(args[1],args[2]):
+                req.put(args)
         else:
-            pass
+            fun = args[0]
+            code = args[1]
+            date = args[2]
+            val = eval(f'{fun}("{code}","{date}")')
+            res.put((fun,code,date,val))
 
-def player(req : Queue):
-    shared = mp.Manager()
-    worker_req = shared.Queue()
-    worker_res = shared.Queue()
-
-    for i in range(os.cpu_count()):
-        process = mp.Process(target=worker,name=f'牛马-{i}',args=[i,worker_req,worker_res],daemon=True)
-        process.start()
-
-    os.makedirs(db_dir,exist_ok=True)
-
+def data_syncing_of_stock_intraday(worker_req : mp.Queue,worker_res : mp.Queue):
     while True:
+        os.makedirs(db_dir,exist_ok=True)
+
         now = datetime.now()
         h9 = now.replace(hour=9, minute=15, second=0)
         h11 = now.replace(hour=11, minute=30, second=0)
@@ -318,37 +316,31 @@ def player(req : Queue):
         h15 = now.replace(hour=15, minute=0, second=0)
         h24 = now.replace(hour=23, minute=59, second=59)
 
-        stocks = get_stock_spot()
-
-        while datetime.now() < h9:
-            worker_req(*req.get())
-        
-        while datetime.now() < h11:
-            worker_req(*req.get())
-
-        while datetime.now() < h13:
-            worker_req(*req.get())
-
         while datetime.now() < h15:
-            worker_req(*req.get())
+            time.sleep(60)
+            continue
         
         stocks = get_stock_spot()
-        
         date = now.strftime('%Y%m%d')
         start = now.replace(hour=9, minute=30, second=0).strftime('%Y-%m-%d %H:%M:%S')
         end = now.replace(hour=9, minute=40, second=0).strftime('%Y-%m-%d %H:%M:%S')
         df = ak.stock_zh_a_hist_min_em(symbol="000001", start_date=start, end_date=end, period="5", adjust="")
-        if not df.empty: stocks.apply(lambda x: worker_req.put(('sync',x['代码'],date,1)),axis=1)
+        if now.weekday() < 5 and not df.empty: stocks.apply(lambda r: worker_req.put(('sync',r['代码'],date)),axis=1)
 
         while datetime.now() < h24:
-            worker_req(*req.get())
-
-from queue import Queue
+            time.sleep(60)
+            continue
 
 if __name__ == '__main__':
-    req = Queue()
+    shared = mp.Manager()
+    worker_req = shared.Queue()
+    worker_res = shared.Queue()
+
+    for i in range(os.cpu_count()):
+        process = mp.Process(target=worker,name=f'牛马-{i}',args=[i,worker_req,worker_res],daemon=True)
+        process.start()
  
-    threading.Thread(target=player,name='玩家',args=[req],daemon=True).start()
+    threading.Thread(target=data_syncing_of_stock_intraday,name='股票数据同步',args=[worker_req,worker_res],daemon=True).start()
 
     while True:
         if len(sys.argv) > 1:
@@ -368,18 +360,18 @@ if __name__ == '__main__':
         parser.add_argument('--days',type=int,default=0)
         
         args = parser.parse_args(cmd)
-        if args.date: date = args.date
-        if args.days: days = args.days
         
-        if args.mode == 'sync':
-            df = pd.DataFrame(dict(zip(records.keys(),records.values())))
-            if df.shape[0]: print(df.to_string())
-            print('-')
-        elif args.mode == 'up':
-            codes = args.code.split(',')
+        if args.mode == 'up':
+            codes = args.code.split(',')            
+            start = datetime.strptime(args.date,'%y%m%d')
+            end = start + timedelta(args.days)
+            if args.days < 0: start,end = end,start
+            dates = [d.strftime('%Y%m%d') for d in pd.date_range(start,end,freq='1D')]
+            up(worker_req,worker_res,dates)
             # if not df.empty: print(df.to_string())
             print('-')
         elif args.mode == 'play':
+            
             print('-')
         else:
             print(params['status'])
